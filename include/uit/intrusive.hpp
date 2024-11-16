50d73b7d901563f11b20777689891a572d6b36ae
TypeCombinator <869119842@qq.com>
2024-11-16 19:06:47 +0800

    Make the code clearer

--- include/uit/intrusive.hpp
+++ include/uit/intrusive.hpp
@@ -4,26 +4,26 @@
 
 namespace uit {
 
-template <std::size_t SL>
+template <std::size_t N>
 struct stag {
     consteval stag() = default;
 
-    consteval stag(char const (&str)[SL]) {
-        for (std::size_t i = 0; i < SL; ++i) {
+    consteval stag(const char (&str)[N]) {
+        for (std::size_t i = 0; i < N; ++i) {
             data[i] = str[i];
         }
     }
 
-    char data[SL];
+    char data[N];
 };
 
-template <typename T, stag N = "">
+template <typename T, stag S = "">
 struct isnode {
     T *right;
 };
 
-template <typename T, stag N = "">
-struct idnode : public isnode<T, N> {
+template <typename T, stag S = "">
+struct idnode : public isnode<T, S> {
     T *left;
 };
 
