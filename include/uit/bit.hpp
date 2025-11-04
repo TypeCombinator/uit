8e4b366c1024b7b6708aafb4ce626337669cf047
TypeCombinator <typecombinator@foxmail.com>
2025-11-04 22:10:46 +0800

    Implement irheap

---
+++ include/uit/bit.hpp
@@ -0,0 +1,31 @@
+// SPDX-FileCopyrightText: 2025 TypeCombinator <typecombinator@foxmail.com>
+//
+// SPDX-License-Identifier: BSD 3-Clause
+
+#ifndef UIT_BIT_3DCA1F74_7932_477A_ABD1_274DBDD21126
+#define UIT_BIT_3DCA1F74_7932_477A_ABD1_274DBDD21126
+
+namespace uit {
+template <typename T, bool enable_ub = false>
+constexpr int countl_zero(T x) noexcept {
+    if constexpr (!enable_ub) {
+        if (x == 0) { // __builtin_clz(0) is UB!
+            return sizeof(x) * 8;
+        }
+    }
+    if constexpr (sizeof(x) < sizeof(unsigned)) {
+        constexpr auto offset = (sizeof(unsigned) - sizeof(x)) * 8;
+        return __builtin_clz(x) - offset;
+    } else if constexpr (sizeof(x) == sizeof(unsigned)) {
+        return __builtin_clz(x);
+    } else if constexpr (sizeof(x) == sizeof(unsigned long)) {
+        return __builtin_clzl(x);
+    } else if constexpr (sizeof(x) == sizeof(unsigned long long)) {
+        return __builtin_clzll(x);
+    } else {
+        static_assert(sizeof(x) <= sizeof(unsigned long long));
+        return -1;
+    }
+}
+} // namespace uit
+#endif // bit.hpp
---
+++ include/uit/irheap.hpp
@@ -0,0 +1,180 @@
+// SPDX-FileCopyrightText: 2025 TypeCombinator <typecombinator@foxmail.com>
+//
+// SPDX-License-Identifier: BSD 3-Clause
+
+#ifndef UIT_IRHEAP_001A9AE4_0EE4_4CF6_9871_6AE428316DD7
+#define UIT_IRHEAP_001A9AE4_0EE4_4CF6_9871_6AE428316DD7
+#include <functional>
+#include <uit/bit.hpp>
+
+namespace uit {
+// An intrusive heap implementation without parent pointers in nodes, it's suitable for scenarios
+// that don't require support for removing arbitrary node but only need to pop the top node.
+
+// Notes:
+// [0]: This implementation doesn't use mock_head, thus there's no risk of UB caused by it, it's
+// safe for use.
+// [1]: If support for arbitrary node removal is required, please refer to this PR:
+// https://github.com/NVIDIA/stdexec/pull/1674
+// [2]: The acronym irheap stands for intrusive recursive heap. While the initial prototype employed
+// recursion, it soon dawned on me that an iterative approach would suffice, rendering recursion
+// entirely unnecessary.
+template <auto Right, auto Left, typename CMP = std::less<>>
+class irheap;
+
+template <typename T, typename MT, MT T::*Right, MT T::*Left, typename CMP>
+class irheap<Right, Left, CMP> {
+   public:
+    using np_t = T *;
+    using m_size_t = std::size_t;
+
+    static constexpr m_size_t path_bit_mask = m_size_t{1} << (sizeof(m_size_t) * 8 - 1);
+
+    // Note: The caller must ensure that the n is non-zero.
+    static m_size_t path_bits(m_size_t n) noexcept {
+        return ((n << 1) | 1u) << uit::countl_zero<m_size_t, true>(n);
+    }
+
+    irheap() noexcept
+        : m_head{nullptr}
+        , m_size{0} {
+    }
+
+    void push(np_t node) noexcept {
+        m_size++;
+        np_t *cur_ptr = &m_head;
+        np_t cur = m_head;
+
+        auto path = path_bits(m_size);
+
+        while (path != path_bit_mask) {
+            if (CMP{}(*node, *cur)) {
+                do { // Replace one by one until reaching the bottom.
+                    node->*Right = cur->*Right;
+                    node->*Left = cur->*Left;
+                    *cur_ptr = node;
+                    if (path & path_bit_mask) {
+                        cur_ptr = &(node->*Right);
+                    } else {
+                        cur_ptr = &(node->*Left);
+                    }
+                    node = cur;
+                    cur = *cur_ptr;
+                    path <<= 1;
+                } while (path != path_bit_mask);
+
+                *cur_ptr = node;
+                node->*Right = nullptr;
+                node->*Left = nullptr;
+                return;
+            }
+            if (path & path_bit_mask) {
+                cur_ptr = &(cur->*Right);
+            } else {
+                cur_ptr = &(cur->*Left);
+            }
+            cur = *cur_ptr;
+            path <<= 1;
+        }
+
+        *cur_ptr = node;
+        node->*Right = nullptr;
+        node->*Left = nullptr;
+    }
+
+    np_t pop() noexcept {
+        np_t top = m_head;
+        if (m_size <= 1u) [[unlikely]] {
+            m_head = nullptr;
+            m_size = 0;
+            return top;
+        }
+        np_t cur = remove_last_leaf(&m_head, m_size);
+        // Replace the top.
+        cur->*Right = top->*Right;
+        cur->*Left = top->*Left;
+        m_head = cur;
+
+        m_size--;
+        sift_down(m_head, &m_head);
+        return top;
+    }
+
+    np_t front() const noexcept {
+        return m_head;
+    }
+
+    bool empty() const noexcept {
+        return m_head == nullptr;
+    }
+
+    auto size() const noexcept {
+        return m_size;
+    }
+   private:
+    static inline void swap_with_right_child(np_t cur, np_t child) noexcept {
+        cur->*Right = child->*Right;
+        child->*Right = cur;
+
+        np_t t = cur->*Left;
+        cur->*Left = child->*Left;
+        child->*Left = t;
+    }
+
+    static inline void swap_with_left_child(np_t cur, np_t child) noexcept {
+        np_t t = cur->*Right;
+        cur->*Right = child->*Right;
+        child->*Right = t;
+
+        cur->*Left = child->*Left;
+        child->*Left = cur;
+    }
+
+    static inline np_t remove_last_leaf(np_t *cur_ptr, m_size_t path) noexcept {
+        np_t cur = *cur_ptr;
+        path = path_bits(path);
+        while (path != path_bit_mask) {
+            if (path & path_bit_mask) {
+                cur_ptr = &(cur->*Right);
+            } else {
+                cur_ptr = &(cur->*Left);
+            }
+            cur = *cur_ptr;
+            path <<= 1;
+        }
+        *cur_ptr = nullptr;
+        return cur;
+    }
+
+    static inline void sift_down(np_t cur, np_t *cur_ptr) noexcept {
+        np_t left = cur->*Left;
+        while (left != nullptr) {
+            np_t right = cur->*Right;
+            if ((right != nullptr) && CMP{}(*right, *left)) {
+                if (CMP{}(*right, *cur)) {
+                    swap_with_right_child(cur, right);
+                    *cur_ptr = right;
+                    cur_ptr = &(right->*Right);
+                } else {
+                    break;
+                }
+            } else {
+                if (CMP{}(*left, *cur)) {
+                    swap_with_left_child(cur, left);
+                    *cur_ptr = left;
+                    cur_ptr = &(left->*Left);
+                } else {
+                    break;
+                }
+            }
+            left = cur->*Left;
+        }
+    }
+
+    T *m_head; // A root pointer.
+    m_size_t m_size;
+};
+
+} // namespace uit
+
+#endif // irheap.hpp
